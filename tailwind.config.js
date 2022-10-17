02c7990a43f03f232fb36ee01b2ab54d0751a9d1
Lazar Stojanović <lakisd98@gmail.com>
2022-10-17 04:14:07 +0200

    Initial Commit
    Finished third of the project before I decided I want this to be a git repo.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/*.{html,js}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
